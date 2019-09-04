26229fbc4cbf04226a3d866fd16e7d3a8eada8fd
PabloChacon1997 <47465321+PabloChacon1997@users.noreply.github.com>
2019-09-04 14:32:32 -0500

    Primera parte

--- src/app/problema2/problema2.component.ts
+++ src/app/problema2/problema2.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Problema2Component implements OnInit {
 
+    valor: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.valor=Math.trunc(Math.random() * 6) + 1;
   }
 
 }
--- src/app/problema3/problema3.component.ts
+++ src/app/problema3/problema3.component.ts
@@ -6,9 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problema3.component.css']
 })
 export class Problema3Component implements OnInit {
+  
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+   }
+   retornarAleatorio(){
+    return Math.trunc( Math.random() * 6) + 1;
+  }
 
-  constructor() { }
-
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3  ) {
+      this.resultado = 'Ganaste';
+    } else {
+      this.resultado = 'Perdiste';
+    }
+  }
   ngOnInit() {
   }
 
