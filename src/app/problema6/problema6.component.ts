ba1521aa6a52df5c7fdee830a6cf1660e363e5e9
PabloChacon1997 <47465321+PabloChacon1997@users.noreply.github.com>
2019-09-08 17:38:10 -0500

    Final

--- src/app/problema6/problema6.component.ts
+++ src/app/problema6/problema6.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Problema6Component implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit() {
+
   }
 
 }
